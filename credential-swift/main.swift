0c4b02a9eb187b214091d4deeb129d57f0f55cd1
Kaz Yoshikawa <kaz@digitallynx.com>
2023-05-29 16:10:45 -0400

    0.9 encryption_key_name option and encrypted_data_name option

--- credential-swift/main.swift
+++ credential-swift/main.swift
@@ -14,7 +14,7 @@ enum CryptoMode: String, CaseIterable, ExpressibleByArgument {
 }
 
 struct credential_swift: ParsableCommand {
-	static let version = "0.8"
+	static let version = "0.9"
 	
 	static let configuration = CommandConfiguration(
 		commandName: "credential-swift",
@@ -25,18 +25,21 @@ struct credential_swift: ParsableCommand {
 	@Argument(help: "`encrypt` or `decrypt` for the input file")
 	var mode: CryptoMode
 	
-	@Option(name: .shortAndLong, help: "Input file path")
+	@Option(name: .long, help: "Input file path")
 	var `in`: String
 	
-	@Option(name: .shortAndLong, help: "Output file path")
+	@Option(name: .long, help: "Output file path")
 	var `out`: String
 	
-	@Option(name: .shortAndLong, help: "Encryption or decryption key file path")
+	@Option(name: .long, help: "Encryption or decryption key file path")
 	var key: String
 	
-	@Option(name: .long, help: "source and identifier of the key")
-	var identifier: String?
-	
+	@Option(name: .long, help: "encryption key name (optional)")
+	var encryption_key_name: String?
+
+	@Option(name: .long, help: "encrypted data name (optional)")
+	var encrypted_data_name: String?
+
 	func run() throws {
 		
 		let keyLength = 32
@@ -60,15 +63,32 @@ struct credential_swift: ParsableCommand {
 			guard keyData.count == keyLength else { throw "`key` must be 32 byte length."  }
 			let outputData = try encrypt_AES256CBC(contentData: inputData, keyData: keyData)
 			try outputData.write(to: URL(fileURLWithPath: `out`))
-			if let identifier = self.identifier {
-				guard isValidIdentifier(identifier) else { throw "not suitable for identifier." }
+
+			if let encryption_key_name = self.encryption_key_name, let encrypted_data_name = self.encrypted_data_name {
+				guard encryption_key_name != encrypted_data_name else { throw "encryption key name and encrypted data name should not be the same." }
+			}
+			if let encryption_key_name = self.encryption_key_name {
+				guard isValidIdentifier(encryption_key_name) else { throw "not suitable for identifier as an encryption key source." }
 				let hexadecimalString = keyData.map { String(format: "0x%02hhx", $0) }.joined(separator: ",")
-				let swiftsource = "let \(identifier) = Data([\(hexadecimalString)])"
-				let swiftfilepath = FileManager.default.currentDirectoryPath.appendingPathComponent(identifier.appendingPathExtension("swift")!)
+
+				let swiftsource = "let \(encryption_key_name) = Data([\(hexadecimalString)])"
+				let swiftfilepath = FileManager.default.currentDirectoryPath.appendingPathComponent(encryption_key_name.appendingPathExtension("swift")!)
 				try swiftsource.data(using: .utf8)!.write(to: URL(filePath: swiftfilepath))
 				
-				let kotlinsource = "val \(identifier) = byteArrayOf(\(hexadecimalString))"
-				let kotlinfilepath = FileManager.default.currentDirectoryPath.appendingPathComponent(identifier.appendingPathExtension("kt")!)
+				let kotlinsource = "val \(encryption_key_name) = byteArrayOf(\(hexadecimalString))"
+				let kotlinfilepath = FileManager.default.currentDirectoryPath.appendingPathComponent(encryption_key_name.appendingPathExtension("kt")!)
+				try kotlinsource.data(using: .utf8)!.write(to: URL(filePath: kotlinfilepath))
+			}
+			if let encrypted_data_name = self.encrypted_data_name {
+				guard isValidIdentifier(encrypted_data_name) else { throw "not suitable for identifier as an encrypted data source." }
+				let hexadecimalString = keyData.map { String(format: "0x%02hhx", $0) }.joined(separator: ",")
+
+				let swiftsource = "let \(encrypted_data_name) = Data([\(hexadecimalString)])"
+				let swiftfilepath = FileManager.default.currentDirectoryPath.appendingPathComponent(encrypted_data_name.appendingPathExtension("swift")!)
+				try swiftsource.data(using: .utf8)!.write(to: URL(filePath: swiftfilepath))
+				
+				let kotlinsource = "val \(encrypted_data_name) = byteArrayOf(\(hexadecimalString))"
+				let kotlinfilepath = FileManager.default.currentDirectoryPath.appendingPathComponent(encrypted_data_name.appendingPathExtension("kt")!)
 				try kotlinsource.data(using: .utf8)!.write(to: URL(filePath: kotlinfilepath))
 			}
 		case .decrypt:
